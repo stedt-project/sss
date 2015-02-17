688829fbfb2231847beb401035bf0d855a662b48
STEDT cgi user <stedt-cgi@corpus.linguistics.Berkeley.EDU>
2015-02-16 23:16:15 -0800

    additional modications to deployment code to support git

--- rootcanal/deployment/make.sh
+++ rootcanal/deployment/make.sh
@@ -1,14 +1,40 @@
 #! /bin/bash
 # use -rtvni to do a dry run with itemized changes
 
-svn up ~/svn-rootcanal
-rsync -rti --exclude '.svn' --exclude 'js' --exclude 'scriptaculous/' --exclude 'admin.tt' ~/svn-rootcanal/web/ ~/public_html
-rsync -rti --exclude '.svn' ~/svn-rootcanal/perl/STEDT/ ~/pm/STEDT
-# add new files to js directory if necessary, but let minify do the replacing
-rsync -rti --exclude '.svn' --ignore-existing --delete ~/svn-rootcanal/web/js/ ~/public_html/js
-rsync -rti --exclude '.svn' --ignore-existing --delete ~/svn-rootcanal/web/scriptaculous/ ~/public_html/scriptaculous/
-find ~/svn-rootcanal/web/js/ -name "*.js" -exec perl ~/svn-rootcanal/deployment/minify.pl {} \;
-find ~/svn-rootcanal/web/scriptaculous/*/ -name "*.js" -exec perl ~/svn-rootcanal/deployment/minify.pl {} \;
+cfgfile=deploy.cfg
+if [ -r $cfgfile ]; then
+	while IFS='= ' read lhs rhs
+	do
+		if [[ ! $lhs =~ ^\ *# && -n $lhs ]]; then
+			rhs="${rhs%%*( )}"   # Del trailing spaces
+			declare $lhs="$rhs"
+		fi
+	done < $cfgfile
+fi
 
-svn info ~/svn-rootcanal | grep 'Revision' > ~/deployed.txt
-perl -pe  's{<\!-- svnversion -->}{"<p>svn revision: " . `svnversion ~/svn-rootcanal/` . "</p>"}e' ~/svn-rootcanal/web/admin.tt > ~/public_html/admin.tt
+if [ ! -d $webdir ]; then
+	echo "No valid web directory specified! (tried '$webdir')"
+	exit 1
+fi
+if [ ! -d $modulesdir ]; then
+	echo "No valid modules directory specified! (tried '$modulesdir')"
+	exit 1
+fi
+
+git pull -v
+rsync -rti --exclude 'js' --exclude 'scriptaculous/' --exclude 'admin.tt' ../web/ $webdir
+rsync -rti ../perl/STEDT/ $modulesdir/STEDT
+if [ $minify = 1 ]; then
+	# add new files to js directory if necessary, but let minify do the replacing
+        echo minifying files in $webdir/js and $webdir/scriptaculous directories...
+	rsync -rti --ignore-existing --delete ../web/js/ $webdir/js
+	rsync -rti --ignore-existing --delete ../web/scriptaculous/ $webdir/scriptaculous
+	find ../web/js -name "*.js" -exec perl minify.pl {} \;
+	find ../web/scriptaculous/* -name "*.js" -exec perl minify.pl {} \;
+else
+        echo deploying unminified files...
+	rsync -rti --delete ../web/js/ $webdir/js
+	rsync -rti --delete ../web/scriptaculous/ $webdir/scriptaculous
+fi
+
+perl -pe 's{<\!-- svnversion -->}{"<p>git revision: " . `git rev-parse --short HEAD` . "</p>"}e' ../web/admin.tt > $webdir/admin.tt
